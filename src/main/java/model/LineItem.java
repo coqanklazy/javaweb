ea9df734751f9e5377259b941d3abba972cba239
coqanklazy <truongconganh5575@gmail.com>
2025-11-20 15:04:25 +0700

    c07

---
+++ src/main/java/model/LineItem.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.io.Serializable;
+import java.text.NumberFormat;
+
+public class LineItem implements Serializable {
+    private Product product;
+    private int quantity;
+
+    public LineItem() {
+    }
+
+    public LineItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        return product.getPrice() * quantity;
+    }
+
+    public String getTotalCurrencyFormat() {
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        return currency.format(getTotal());
+    }
+}
